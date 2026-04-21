3b9e7cb1f110e961bd8913173310337feb5d36cf
Nghi To <nghitb@gt.tech.gov.sg>
2026-04-21 08:44:03 +0700

    [BOOKINGSG-9224][Nghi] convert styled components to linaria css for tag

--- src/tag/tag.style.ts
+++ src/tag/tag.style.ts
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import { css } from "@linaria/core";
 
 import { Border, Colour, Font, MediaQuery, Radius } from "../theme";
 
@@ -6,7 +6,16 @@ import { Border, Colour, Font, MediaQuery, Radius } from "../theme";
 // STYLING
 // =============================================================================
 
-export const Wrapper = styled.div`
+export const wrapperInteractive = css`
+    cursor: pointer;
+
+    ${MediaQuery.MaxWidth.lg} {
+        ${Font["body-md-semibold"]}
+        padding: calc(0.5rem - 1px) 1rem;
+    }
+`;
+
+export const wrapper = css`
     border-radius: ${Radius["sm"]};
     padding: 0.125rem 0.5rem;
     width: fit-content;
@@ -21,257 +30,205 @@ export const Wrapper = styled.div`
     svg {
         flex-shrink: 0;
     }
+`;
 
-    &.wrapperSolidGrey {
-        background: ${Colour["bg-inverse-subtler"]};
-        border: ${Border["width-010"]} ${Border.solid} transparent;
-        color: ${Colour["text-inverse"]};
+export const wrapperSolidGrey = css`
+    background: ${Colour["bg-inverse-subtler"]};
+    border: ${Border["width-010"]} ${Border.solid} transparent;
+    color: ${Colour["text-inverse"]};
 
-        &.wrapperInteractive {
-            cursor: pointer;
-
-            @media (hover: hover) {
-                &:hover {
-                    background: ${Colour["bg-inverse-hover"]};
-                }
+    &.${wrapperInteractive} {
+        @media (hover: hover) {
+            &:hover {
+                background: ${Colour["bg-inverse-hover"]};
             }
-        }
-    }
-
-    /* Solid type - green */
-    &.wrapperSolidGreen {
-        background: ${Colour["bg-success-strong"]};
-        border: ${Border["width-010"]} ${Border.solid} transparent;
-        color: ${Colour["text-inverse"]};
-
-        &.wrapperInteractive {
-            cursor: pointer;
-
-            @media (hover: hover) {
-                &:hover {
-                    background: ${Colour["bg-success-strong-hover"]};
-                }
-            }
-        }
-    }
-
-    /* Solid type - red */
-    &.wrapperSolidRed {
-        background: ${Colour["bg-error-strong"]};
-        border: ${Border["width-010"]} ${Border.solid} transparent;
-        color: ${Colour["text-inverse"]};
-
-        &.wrapperInteractive {
-            cursor: pointer;
-
-            @media (hover: hover) {
-                &:hover {
-                    background: ${Colour["bg-error-strong-hover"]};
-                }
-            }
-        }
-    }
-
-    /* Solid type - blue */
-    &.wrapperSolidBlue {
-        background: ${Colour["bg-info-strong"]};
-        border: ${Border["width-010"]} ${Border.solid} transparent;
-        color: ${Colour["text-inverse"]};
-
-        &.wrapperInteractive {
-            cursor: pointer;
-
-            @media (hover: hover) {
-                &:hover {
-                    background: ${Colour["bg-info-strong-hover"]};
-                }
-            }
-        }
-    }
-
-    /* Solid type - primary */
-    &.wrapperSolidPrimary {
-        background: ${Colour["bg-primary"]};
-        border: ${Border["width-010"]} ${Border.solid} transparent;
-        color: ${Colour["text-inverse"]};
-
-        &.wrapperInteractive {
-            cursor: pointer;
-
-            @media (hover: hover) {
-                &:hover {
-                    background: ${Colour["bg-primary-hover"]};
-                }
-            }
-        }
-    }
-
-    /* Solid type - yellow */
-    &.wrapperSolidYellow {
-        background: ${Colour["bg-warning-strong"]};
-        border: ${Border["width-010"]} ${Border.solid} transparent;
-        color: ${Colour["text-inverse"]};
-
-        &.wrapperInteractive {
-            cursor: pointer;
-
-            @media (hover: hover) {
-                &:hover {
-                    background: ${Colour["bg-warning-strong-hover"]};
-                }
-            }
-        }
-    }
-
-    /* Solid type - default (black) */
-    &.wrapperSolidBlack {
-        background: ${Colour["bg-inverse"]};
-        border: ${Border["width-010"]} ${Border.solid} transparent;
-        color: ${Colour["text-inverse"]};
-
-        &.wrapperInteractive {
-            cursor: pointer;
-
-            @media (hover: hover) {
-                &:hover {
-                    background: ${Colour["bg-inverse-hover"]};
-                }
-            }
-        }
-    }
-
-    /* Outline type - grey */
-    &.wrapperOutlineGrey {
-        background: ${Colour["bg-strong"]};
-        border: ${Border["width-010"]} ${Border.solid}
-            ${Colour["border-strong"]};
-        color: ${Colour["text-subtle"]};
-
-        &.wrapperInteractive {
-            cursor: pointer;
-
-            @media (hover: hover) {
-                &:hover {
-                    background: ${Colour["bg-hover-neutral-strong"]};
-                }
-            }
-        }
-    }
-
-    /* Outline type - green */
-    &.wrapperOutlineGreen {
-        background: ${Colour["bg-success"]};
-        border: ${Border["width-010"]} ${Border.solid}
-            ${Colour["border-success"]};
-        color: ${Colour["text-success"]};
-
-        &.wrapperInteractive {
-            cursor: pointer;
-
-            @media (hover: hover) {
-                &:hover {
-                    background: ${Colour["bg-success-hover"]};
-                }
-            }
-        }
-    }
-
-    /* Outline type - yellow */
-    &.wrapperOutlineYellow {
-        background: ${Colour["bg-warning"]};
-        border: ${Border["width-010"]} ${Border.solid}
-            ${Colour["border-warning"]};
-        color: ${Colour["text-warning"]};
-
-        &.wrapperInteractive {
-            cursor: pointer;
-
-            @media (hover: hover) {
-                &:hover {
-                    background: ${Colour["bg-warning-hover"]};
-                }
-            }
-        }
-    }
-
-    /* Outline type - red */
-    &.wrapperOutlineRed {
-        background: ${Colour["bg-error"]};
-        border: ${Border["width-010"]} ${Border.solid} ${Colour["border-error"]};
-        color: ${Colour["text-error"]};
-
-        &.wrapperInteractive {
-            cursor: pointer;
-
-            @media (hover: hover) {
-                &:hover {
-                    background: ${Colour["bg-error-hover"]};
-                }
-            }
-        }
-    }
-
-    /* Outline type - blue */
-    &.wrapperOutlineBlue {
-        background: ${Colour["bg-info"]};
-        border: ${Border["width-010"]} ${Border.solid} ${Colour["border-info"]};
-        color: ${Colour["text-info"]};
-
-        &.wrapperInteractive {
-            cursor: pointer;
-
-            @media (hover: hover) {
-                &:hover {
-                    background: ${Colour["bg-info-hover"]};
-                }
-            }
-        }
-    }
-
-    /* Outline type - primary */
-    &.wrapperOutlinePrimary {
-        background: ${Colour["bg-primary-subtlest"]};
-        border: ${Border["width-010"]} ${Border.solid}
-            ${Colour["border-primary"]};
-        color: ${Colour["text-primary"]};
-
-        &.wrapperInteractive {
-            cursor: pointer;
-
-            @media (hover: hover) {
-                &:hover {
-                    background: ${Colour["bg-primary-hover"]};
-                }
-            }
-        }
-    }
-
-    /* Outline type - default (black) */
-    &.wrapperOutlineBlack {
-        background: ${Colour.bg};
-        border: ${Border["width-010"]} ${Border.solid} ${Colour.border};
-        color: ${Colour.text};
-
-        &.wrapperInteractive {
-            cursor: pointer;
-
-            @media (hover: hover) {
-                &:hover {
-                    background: ${Colour["bg-hover-neutral"]};
-                }
-            }
-        }
-    }
-
-    ${MediaQuery.MaxWidth.lg} {
-        &.wrapperInteractive {
-            ${Font["body-md-semibold"]}
-            padding: calc(0.5rem - 1px) 1rem;
         }
     }
 `;
 
-export const Label = styled.div`
+export const wrapperSolidGreen = css`
+    background: ${Colour["bg-success-strong"]};
+    border: ${Border["width-010"]} ${Border.solid} transparent;
+    color: ${Colour["text-inverse"]};
+
+    &.${wrapperInteractive} {
+        @media (hover: hover) {
+            &:hover {
+                background: ${Colour["bg-success-strong-hover"]};
+            }
+        }
+    }
+`;
+
+export const wrapperSolidYellow = css`
+    background: ${Colour["bg-warning-strong"]};
+    border: ${Border["width-010"]} ${Border.solid} transparent;
+    color: ${Colour["text-inverse"]};
+
+    &.${wrapperInteractive} {
+        @media (hover: hover) {
+            &:hover {
+                background: ${Colour["bg-warning-strong-hover"]};
+            }
+        }
+    }
+`;
+
+export const wrapperSolidRed = css`
+    background: ${Colour["bg-error-strong"]};
+    border: ${Border["width-010"]} ${Border.solid} transparent;
+    color: ${Colour["text-inverse"]};
+
+    &.${wrapperInteractive} {
+        @media (hover: hover) {
+            &:hover {
+                background: ${Colour["bg-error-strong-hover"]};
+            }
+        }
+    }
+`;
+
+export const wrapperSolidBlue = css`
+    background: ${Colour["bg-info-strong"]};
+    border: ${Border["width-010"]} ${Border.solid} transparent;
+    color: ${Colour["text-inverse"]};
+
+    &.${wrapperInteractive} {
+        @media (hover: hover) {
+            &:hover {
+                background: ${Colour["bg-info-strong-hover"]};
+            }
+        }
+    }
+`;
+
+export const wrapperSolidPrimary = css`
+    background: ${Colour["bg-primary"]};
+    border: ${Border["width-010"]} ${Border.solid} transparent;
+    color: ${Colour["text-inverse"]};
+
+    &.${wrapperInteractive} {
+        @media (hover: hover) {
+            &:hover {
+                background: ${Colour["bg-primary-hover"]};
+            }
+        }
+    }
+`;
+
+export const wrapperSolidBlack = css`
+    background: ${Colour["bg-inverse"]};
+    border: ${Border["width-010"]} ${Border.solid} transparent;
+    color: ${Colour["text-inverse"]};
+
+    &.${wrapperInteractive} {
+        @media (hover: hover) {
+            &:hover {
+                background: ${Colour["bg-inverse-hover"]};
+            }
+        }
+    }
+`;
+
+export const wrapperOutlineGrey = css`
+    background: ${Colour["bg-strong"]};
+    border: ${Border["width-010"]} ${Border.solid} ${Colour["border-strong"]};
+    color: ${Colour["text-subtle"]};
+
+    &.${wrapperInteractive} {
+        @media (hover: hover) {
+            &:hover {
+                background: ${Colour["bg-hover-neutral-strong"]};
+            }
+        }
+    }
+`;
+
+export const wrapperOutlineGreen = css`
+    background: ${Colour["bg-success"]};
+    border: ${Border["width-010"]} ${Border.solid} ${Colour["border-success"]};
+    color: ${Colour["text-success"]};
+
+    &.${wrapperInteractive} {
+        @media (hover: hover) {
+            &:hover {
+                background: ${Colour["bg-success-hover"]};
+            }
+        }
+    }
+`;
+
+export const wrapperOutlineYellow = css`
+    background: ${Colour["bg-warning"]};
+    border: ${Border["width-010"]} ${Border.solid} ${Colour["border-warning"]};
+    color: ${Colour["text-warning"]};
+
+    &.${wrapperInteractive} {
+        @media (hover: hover) {
+            &:hover {
+                background: ${Colour["bg-warning-hover"]};
+            }
+        }
+    }
+`;
+
+export const wrapperOutlineRed = css`
+    background: ${Colour["bg-error"]};
+    border: ${Border["width-010"]} ${Border.solid} ${Colour["border-error"]};
+    color: ${Colour["text-error"]};
+
+    &.${wrapperInteractive} {
+        @media (hover: hover) {
+            &:hover {
+                background: ${Colour["bg-error-hover"]};
+            }
+        }
+    }
+`;
+
+export const wrapperOutlineBlue = css`
+    background: ${Colour["bg-info"]};
+    border: ${Border["width-010"]} ${Border.solid} ${Colour["border-info"]};
+    color: ${Colour["text-info"]};
+
+    &.${wrapperInteractive} {
+        @media (hover: hover) {
+            &:hover {
+                background: ${Colour["bg-info-hover"]};
+            }
+        }
+    }
+`;
+
+export const wrapperOutlinePrimary = css`
+    background: ${Colour["bg-primary-subtlest"]};
+    border: ${Border["width-010"]} ${Border.solid} ${Colour["border-primary"]};
+    color: ${Colour["text-primary"]};
+
+    &.${wrapperInteractive} {
+        @media (hover: hover) {
+            &:hover {
+                background: ${Colour["bg-primary-hover"]};
+            }
+        }
+    }
+`;
+
+export const wrapperOutlineBlack = css`
+    background: ${Colour.bg};
+    border: ${Border["width-010"]} ${Border.solid} ${Colour.border};
+    color: ${Colour.text};
+
+    &.${wrapperInteractive} {
+        @media (hover: hover) {
+            &:hover {
+                background: ${Colour["bg-hover-neutral"]};
+            }
+        }
+    }
+`;
+
+export const label = css`
     overflow: hidden;
     display: -webkit-box;
     -webkit-box-orient: vertical;
--- src/tag/tag.tsx
+++ src/tag/tag.tsx
@@ -1,8 +1,29 @@
 import clsx from "clsx";
 import React from "react";
 
-import { Label, Wrapper } from "./tag.style";
-import type { TagProps } from "./types";
+import * as styles from "./tag.style";
+import type { TagColorType, TagProps, TagType } from "./types";
+
+const colorClassMap: Record<TagType, Record<TagColorType, string>> = {
+    solid: {
+        grey: styles.wrapperSolidGrey,
+        green: styles.wrapperSolidGreen,
+        yellow: styles.wrapperSolidYellow,
+        red: styles.wrapperSolidRed,
+        blue: styles.wrapperSolidBlue,
+        primary: styles.wrapperSolidPrimary,
+        black: styles.wrapperSolidBlack,
+    },
+    outline: {
+        grey: styles.wrapperOutlineGrey,
+        green: styles.wrapperOutlineGreen,
+        yellow: styles.wrapperOutlineYellow,
+        red: styles.wrapperOutlineRed,
+        blue: styles.wrapperOutlineBlue,
+        primary: styles.wrapperOutlinePrimary,
+        black: styles.wrapperOutlineBlack,
+    },
+};
 
 export const Tag = ({
     type,
@@ -13,29 +34,26 @@ export const Tag = ({
     iconPosition = "left",
     ...otherProps
 }: TagProps): JSX.Element => {
-    const getWrapperClassName = () => {
-        const baseClass = `wrapper${type.charAt(0).toUpperCase()}${type.slice(
-            1
-        )}`;
-        const colorClass = `${baseClass}${colorType
-            .charAt(0)
-            .toUpperCase()}${colorType.slice(1)}`;
-        return clsx(colorClass, interactive && "wrapperInteractive");
-    };
+    const Element = interactive ? "button" : "div";
 
     return (
-        <Wrapper
-            as={interactive ? "button" : "div"}
-            className={getWrapperClassName()}
+        <Element
+            className={clsx(
+                styles.wrapper,
+                colorClassMap[type]?.[colorType],
+                interactive && styles.wrapperInteractive
+            )}
             {...otherProps}
         >
             {iconPosition === "left" &&
                 icon &&
                 React.cloneElement(icon, { "aria-hidden": true })}
-            <Label role="presentation">{children}</Label>
+            <div className={styles.label} role="presentation">
+                {children}
+            </div>
             {iconPosition === "right" &&
                 icon &&
                 React.cloneElement(icon, { "aria-hidden": true })}
-        </Wrapper>
+        </Element>
     );
 };
