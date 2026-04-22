c8a3436e1864e050d915b75c10a54d3251ecb8e6
Ryan.Nguyen <trungnh41@fpt.com>
2026-04-22 19:07:36 +0700

    [BOOKINGSG-9152][RYN] add style class for label error message

--- src/form/form-label.tsx
+++ src/form/form-label.tsx
@@ -65,7 +65,7 @@ export const FormErrorMessage = (
                 className={styles.errorIcon}
                 aria-hidden
             />
-            <p {...props} />
+            <p className={styles.errorMessage} {...props} />
         </div>
     );
 };
